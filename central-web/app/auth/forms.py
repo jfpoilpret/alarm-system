b574d4954e36c6a810158a94bb49455a12db9999
Jean-François Poilpret <jfpoilpret@gmail.com>
2015-04-26 17:19:43 +0200

    Added UI to change current user profile or password.

--- central-web/app/auth/forms.py
+++ central-web/app/auth/forms.py
@@ -1,9 +1,19 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Required, Length
+from wtforms.validators import Required, Length, DataRequired
 
 class SigninForm(Form):
     username = StringField('User Name', validators=[Required(), Length(1, 64)])
     password = PasswordField('Password')
     submit = SubmitField('Sign In')
 
+# Form to edit user profile
+class ProfileForm(Form):
+    username = StringField('User Name', validators=[DataRequired(), Length(3, 64)])
+    fullname = StringField('Full Name', validators=[DataRequired(), Length(4, 128)])
+    submit = SubmitField('Save Profile')
+
+class PasswordForm(Form):
+    password = PasswordField('New Password', validators=[DataRequired(), Length(3, 64)])
+    repeat_password = PasswordField('Repeat Password', validators=[DataRequired(), Length(3, 64)])
+    submit = SubmitField('Save Password')
--- central-web/app/auth/routes.py
+++ central-web/app/auth/routes.py
@@ -1,9 +1,11 @@
 from flask import flash, redirect, render_template, url_for
-from flask_login import login_user, logout_user, login_required
-from . import auth
-from .forms import SigninForm
+from flask_login import login_user, logout_user, login_required, current_user
+from app.auth import auth
+from app.auth.forms import SigninForm
 
-from ..models import Account
+from app.models import Account
+from app.auth.forms import ProfileForm, PasswordForm
+from app import db
 
 def renderSignInPage(signinForm = None):
     if not signinForm:
@@ -32,3 +34,33 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('.login'))
+
+@auth.route('/profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    profileForm = ProfileForm(prefix = 'profile_', obj = current_user)
+    if profileForm.validate_on_submit():
+        profileForm.populate_obj(current_user)
+        db.session.add(current_user)
+        db.session.commit()
+        flash('Your profile has been saved', 'success')
+        return redirect(url_for('configure.home'))
+    return render_template('auth/profile.html', profileForm = profileForm)
+
+@auth.route('/password', methods=['GET', 'POST'])
+@login_required
+def change_password():
+    passwordForm = PasswordForm(prefix = 'password_')
+    if passwordForm.validate_on_submit():
+        # check both passwords are the same
+        pw1 = passwordForm.password.data
+        pw2 = passwordForm.repeat_password.data
+        if pw1 == pw2:
+            current_user.password = pw1
+            db.session.add(current_user)
+            db.session.commit()
+            flash('Your password has been changed', 'success')
+            return redirect(url_for('configure.home'))
+        else:
+            flash('Please ensure you typed the same password twice!', 'warning')
+    return render_template('auth/password.html', passwordForm = passwordForm)
