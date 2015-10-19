6fd7ef07ab757f8aefc5ffd50264dc98131acf34
Jean-François Poilpret <jfpoilpret@gmail.com>
2015-10-19 21:19:09 +0200

    Start refactoring NRF24 handling through separate C++ process.

---
+++ central-rf/RFManager/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Jean-François
+ *
+ * Created on 19. octobre 2015, 21:13
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+	return 0;
+}
+
