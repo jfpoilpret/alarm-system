abc3e04634852f2ad65a72154cc82e2a7151359c
Jean-François Poilpret <jfpoilpret@gmail.com>
2015-12-19 22:33:25 +0100

    Second step in porting Arduino work to netbeans: replace ActivationModule project config.

---
+++ modules/ActivationModule/nbproject/Makefile-ATmega328-Release.mk
@@ -0,0 +1,88 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=avr-gcc
+CCC=avr-g++
+CXX=avr-g++
+FC=gfortran
+AS=avr-as
+
+# Macros
+CND_PLATFORM=Arduino-1.6.6-Windows
+CND_DLIB_EXT=dll
+CND_CONF=ATmega328-Release
+CND_DISTDIR=dist
+CND_BUILDDIR=build
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/ActivationModule.o
+
+
+# C Compiler Flags
+CFLAGS=-mmcu=${MCU} -DF_CPU=${F_CPU} -DARDUINO=${ARDUINO} -Wextra -flto -g -Os -ffunction-sections -fdata-sections
+
+# CC Compiler Flags
+CCFLAGS=-mmcu=${MCU} -DF_CPU=${F_CPU} -DARDUINO=${ARDUINO} -fno-exceptions -Wextra -flto -std=gnu++11 -felide-constructors -g -Os -ffunction-sections -fdata-sections
+CXXFLAGS=-mmcu=${MCU} -DF_CPU=${F_CPU} -DARDUINO=${ARDUINO} -fno-exceptions -Wextra -flto -std=gnu++11 -felide-constructors -g -Os -ffunction-sections -fdata-sections
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=../arduino_cosa_lib/dist/ATmega328-Release/Arduino-1.6.6-Windows/libarduino_cosa_lib.a
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/activationmodule.exe
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/activationmodule.exe: ../arduino_cosa_lib/dist/ATmega328-Release/Arduino-1.6.6-Windows/libarduino_cosa_lib.a
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/activationmodule.exe: ${OBJECTFILES}
+	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
+	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/activationmodule ${OBJECTFILES} ${LDLIBSOPTIONS} -Os -Wl,--gc-sections -Wl,-relax -flto -mmcu=${MCU} -lm
+
+${OBJECTDIR}/ActivationModule.o: ActivationModule.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -Wall -I../../../../arduino/tools/arduino-1.5.8/sketchbook/hardware/Cosa/avr/cores/cosa -I../../../../arduino/tools/arduino-1.5.8/sketchbook/hardware/Cosa/avr/variants/${VARIANT} -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/ActivationModule.o ActivationModule.cpp
+
+# Subprojects
+.build-subprojects:
+	cd ../arduino_cosa_lib && ${MAKE}  -f Makefile CONF=ATmega328-Release
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/activationmodule.exe
+
+# Subprojects
+.clean-subprojects:
+	cd ../arduino_cosa_lib && ${MAKE}  -f Makefile CONF=ATmega328-Release clean
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
