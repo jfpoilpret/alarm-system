97f884dbb45a75d669bc45dc318693874a2e893e
Jean-François Poilpret <jfpoilpret@gmail.com>
2016-01-20 21:22:44 +0100

    Draft work on identification through OneWire; optimize code size 5not enough to fit ATtiny yet).

--- modules/ModuleCommons/UniqueId.cpp
+++ modules/ModuleCommons/UniqueId.cpp
@@ -8,9 +8,69 @@ UniqueId::UniqueId(Board::DigitalPin pin):
 void UniqueId::get_id(uint8_t id[UniqueId::ID_SIZE])
 {
 	cli();
-	_reset();
-	_write_byte(READ_ROM);
-	_read(id, ID_SIZE);
+	uint8_t retry = 4;
+	uint8_t res = 0;
+	do
+	{
+		_output();
+		_set();
+		_clear();
+		DELAY(480);
+		_set();
+		_input();
+		DELAY(70);
+		res = _is_clear();
+		DELAY(410);
+	}
+	while (retry-- && !res);
+	uint8_t value = READ_ROM;
+	_output();
+	_set();
+	for (uint8_t bit = 0; bit < CHARBITS; bit++)
+	{
+		_clear();
+		if (value & 1)
+		{
+			DELAY(6);
+			_set();
+			DELAY(64);
+		}
+		else
+		{
+			DELAY(60);
+			_set();
+			DELAY(10);
+		}
+		value >>= 1;
+	}
+	_input();
+	_clear();
+	
+//	uint8_t* bp = (uint8_t*) id;
+	uint8_t size = ID_SIZE;
+	while (size--)
+	{
+		uint8_t res = 0;
+		for (uint8_t bit = 0; bit < CHARBITS; bit++)
+		{
+			_output();
+			_set();
+			_clear();
+			DELAY(6);
+			_input();
+			DELAY(9);
+			res >>= 1;
+			if (_is_set())
+				res |= 0x80;
+			DELAY(55);
+		}
+		*id++ = res;
+//		*bp++ = res;
+//		*bp++ = _read_byte();
+	}
+//	_reset();
+//	_write_byte(READ_ROM);
+//	_read(id, ID_SIZE);
 	sei();
 }
 
